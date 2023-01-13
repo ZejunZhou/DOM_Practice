24e59b21aba6f263594858463ae60143b6cc74c5
ZejunZhou <zzhou443>
2023-01-13 18:54:55 +0800

    updating dom js attributes text-content notes

--- DOM-Challenge/index.js
+++ DOM-Challenge/index.js
@@ -4,3 +4,7 @@ document.getElementsByTagName("h1")[0].style.color = "blue";
 
 document.querySelector("h1").classList.add("underline");
 
+document.querySelector("button").textContent;
+
+document.querySelector("a").getAttribute;
+
